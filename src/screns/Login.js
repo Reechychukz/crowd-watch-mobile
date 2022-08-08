6f2462e3caa751ab3c22e4102857bd00085dba7a
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2022-08-08 19:01:14 +0100

    added new screens views

---
+++ src/screns/Login.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+
+//formik
+import { Formik } from 'formik';
+
+//icons
+import { Octicons } from '@expo/vector-icons';
+
+import {
+    StyledContainer,
+    InnerContainer,
+    PageLogo,
+    PageTitle,
+    SubTitle,
+    StyledFormArea,
+    LeftIcon,
+    StyledInputLabel,
+    StyledTextInput,
+    RightIcon,
+    Colors
+} from "../components/style";
+
+//Colors
+const { brand, darkLight } = Colors;
+
+const Login = () => {
+    return (
+        <StyledContainer>
+            <StatusBar style="dark" />
+            <InnerContainer>
+                <PageLogo resizeMode="cover" source={require('../../assets/img/login.jpg')}></PageLogo>
+                <PageTitle>Crowd Watch</PageTitle>
+                <SubTitle>Login</SubTitle>
+                <Formik
+                    initialValues={{ email: '', password: '' }}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                >
+                    {({ handleChange, handleBlur, handleSubmit, values }) => (
+                        <StyledFormArea>
+                            <MyTextInput
+                                label="Email Address"
+                                icon="mail"
+                                placeholder="johndoe@mail.com"
+                                placeholderTextColor={darkLight}
+                                onChangeText={handleChange('email')}
+                                onBlur={handleBlur('email')}
+                                value={values.email}
+                                keyboardType="email-address"
+                            />
+
+                            <MyTextInput
+                                label="Password"
+                                icon="lock"
+                                placeholder="* * * * * * * *"
+                                placeholderTextColor={darkLight}
+                                onChangeText={handleChange('password')}
+                                onBlur={handleBlur('password')}
+                                value={values.password}
+                                secureTextEntry={true}
+                            />
+                        </StyledFormArea>)}
+                </Formik>
+            </InnerContainer>
+        </StyledContainer>
+    )
+}
+
+const MyTextInput = ({ label, icon, ...props }) => {
+    return (
+        <View>
+            <LeftIcon>
+                <Octicons name={icon} size={30} color={brand} />
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+            <StyledTextInput {...props} />
+        </View>
+    )
+}
+
+export default Login;
---
+++ src/screns/SignUp.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SignUp() {
+  return (
+    <View style={styles.container}>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    
+  },
+});
