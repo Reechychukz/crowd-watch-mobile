690f29a2e129a22fd60767d268d984d981384b08
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2023-01-14 23:47:25 +0100

    set up drawer stack and drawer content screen

---
+++ src/navigators/DrawerStack.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import 'react-native-gesture-handler';
+
+import TabStack from './TabStack';
+import Home from '../screens/Home';
+import { DrawerContent } from '../screens/DrawerContent';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
+
+
+const DrawerStack = () => {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{ headerShown: false }} drawerContent={props => <DrawerContent {...props} />}>
+        <Drawer.Screen name='DrawerStack' component={TabStack} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default DrawerStack
