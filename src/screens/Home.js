84659f25dd22084657c994e71535c13b6570a335
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2023-01-18 15:42:15 +0100

    Home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,62 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React from 'react';
+import Constants from 'expo-constants';
+
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  ScrollView
+} from 'react-native';
+
+
+import { FontAwesome } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+
 import KeyboardAvoidingWrapper from '../components/KeyboardAvoidingWrapper'
 import { InnerContainer, PageTitle, StyledContainer } from '../components/style';
+import Posts from '../components/Posts';
 
-const Home = () => {
+const Home = ({ navigation }) => {
   return (
-    <KeyboardAvoidingWrapper>
-        <StyledContainer>
-            <StatusBar style='dark' />
-            <InnerContainer>
-                <PageTitle>Home</PageTitle>
-            </InnerContainer>
-        </StyledContainer>
-    </KeyboardAvoidingWrapper>
+    <View style={styles.homeLayout}>
+      <StatusBar backgroundColor='white' barStyle='dark-content' animated={true} />
+      <View style={styles.header}>
+        <FontAwesome name="navicon" size={24} color="black" onPress={() => navigation.openDrawer()} />
+        <Text style={{ fontFamily: 'roboto-regular', fontSize: 22 }}>
+          News Feed
+        </Text>
+        <FontAwesome name="plus-square-o" size={24} color="black" />
+      </View>
+      <View style={{
+
+      }}>
+      </View>
+
+      <ScrollView>
+        <Posts />
+      </ScrollView>
+    </View>
   )
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  homeLayout: {
+    backgroundColor: 'white',
+    height: '100%'
+  },
+  container: {
+    flex: 1,
+  },
+  header: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    alignItems: 'center',
+    marginTop: Constants.statusBarHeight + 15
+  }
+})
