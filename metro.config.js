5b5b2dde23a8604143dfd204eb9ce645f5e84189
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2023-03-15 01:21:55 +0100

    metro config to support app.config

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
-const { getDefaultConfig } = require("@expo/metro-config");
+const { getDefaultConfig } = require('@expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
-
-defaultConfig.resolver.assetExts.push("cjs");
+console.log(__dirname);
+defaultConfig.resolver.assetExts.push('cjs');
 
 module.exports = defaultConfig;
