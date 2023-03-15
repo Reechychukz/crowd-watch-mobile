13de2100007578d7ca2239589b8296574d91cb99
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2023-03-15 01:24:19 +0100

    removed navigator container

--- src/navigators/RootStack.js
+++ src/navigators/RootStack.js
@@ -1,29 +1,26 @@
 import React from 'react';
 
 import { Colors } from '../components/style';
-const { primary, tertiary } = Colors;
 
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 //screens
 import Login from '../screens/Login';
 import Signup from '../screens/SignUp';
 import Verification from '../screens/OtpVerification';
-import Welcome from '../screens/Welcome';
+
+const { primary, tertiary } = Colors;
 
 const Stack = createNativeStackNavigator();
 
 //Credentials context
 
 import { CredentialsContext } from '../components/CredentialsContext';
-import Tabs from './TabStack';
 
 const RootStack = () => {
     return (
         <CredentialsContext.Consumer>
             {({ storedCredentials }) => (
-                <NavigationContainer>
                     <Stack.Navigator
                         screenOptions={{
                             headerStyle: {
@@ -37,11 +34,11 @@ const RootStack = () => {
                             }
 
                         }}
-                        initialRouteName='Verification'
+                        initialRouteName='Login'
                     >
                         {storedCredentials ?
                             <Stack.Screen options={{ headerTintColor: primary }} 
-                            name="Welcome" component={Welcome} />
+                            name="Signup" component={Signup} />
                             : <>
                                 <Stack.Screen name="Login" component={Login} />
                                 <Stack.Screen name="Signup" component={Signup} />
@@ -50,7 +47,6 @@ const RootStack = () => {
                         }
 
                     </Stack.Navigator>
-                </NavigationContainer>
             )}
         </CredentialsContext.Consumer>
 
