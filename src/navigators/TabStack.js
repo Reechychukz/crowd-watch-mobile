ddbb5a54e306589e299b0df77e5940ab0b6d2a89
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2023-04-18 23:54:00 +0100

    udated tab stack

--- src/navigators/TabStack.js
+++ src/navigators/TabStack.js
@@ -2,74 +2,46 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import Home from '../screens/Home';
-import Emergency from '../screens/Emergency';
-import Friends from '../screens/Friends';
-import Profile from '../screens/Profile';
 import TapBar from '../components/TapBar';
-import { NavigationContainer } from '@react-navigation/native';
 import ProfileStack from './ProfileStack';
+import HomeStack from './HomeStack';
 
 const Tab = createBottomTabNavigator();
 
 const TabStack = () => {
     return (
-        // <NavigationContainer>
-            <Tab.Navigator
-                initialRouteName='Emergency'
-                headerShown='false'
+        <Tab.Navigator
+            initialRouteName='Home'
+            headerShown='true'
 
-                tapBar={props => <TapBar {...props} />}>
-                <Tab.Screen
-                    name='Home'
-                    component={Home}
-                    options={{
-                        headerShown: false,
-                        tabBarLabel: 'Home',
-                        tabBarShowLabel: false,
-                        tabBarIcon: ({ color, size }) => (
-                            <MaterialCommunityIcons name="home" color={color} size={size} />
-                        )
-                    }} />
+            tapBar={props => <TapBar {...props} />}>
+            <Tab.Screen
+                name='Home'
+                component={HomeStack}
+                options={{
+                    headerShown: false,
+                    headerShadowVisible: false,
+                    tabBarLabel: 'Home',
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={size} />
+                    )
+                }} />
 
-                <Tab.Screen
-                    name='Emergency'
-                    component={Emergency}
-                    options={{
-                        tabBarLabel: 'Emergency',
-                        tabBarShowLabel: false,
-                        tabBarIcon: ({ color, size }) => (
-                            <MaterialCommunityIcons name="alert-circle" color={color} size={size} />
-                        )
-                    }}
-                />
-                <Tab.Screen
-                    name='Friends'
-                    component={Friends}
-                    options={{
-                        tabBarLabel: 'Friends',
-                        tabBarShowLabel: false,
-                        tabBarIcon: ({ color, size }) => (
-                            <MaterialCommunityIcons name="account-group" color={color} size={size} />
-                        )
-                    }}
-                />
-
-                <Tab.Screen
-                    name='Profile'
-                    component={ProfileStack}
-                    options={{
-                        headerShown: false,
-                        headerShadowVisible: false,
-                        tabBarShowLabel: false,
-                        tabBarLabel: 'Profile',
-                        tabBarIcon: ({ color, size }) => (
-                            <MaterialCommunityIcons name="account" color={color} size={size} />
-                        )
-                    }}
-                />
-            </Tab.Navigator>
-        // </NavigationContainer>
+            <Tab.Screen
+                name='Profile'
+                component={ProfileStack}
+                options={{
+                    headerShown: false,
+                    headerShadowVisible: false,
+                    tabBarShowLabel: false,
+                    tabBarLabel: 'Profile',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="account" color={color} size={size} />
+                    )
+                }}
+            />
+        </Tab.Navigator>
     )
 }
 
