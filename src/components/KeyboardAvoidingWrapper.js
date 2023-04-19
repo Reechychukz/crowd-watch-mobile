251c24227be26adf1bd09556b56d5a8e61f1bd9f
Richard Chukwuma <45286636+Reechychukz@users.noreply.github.com>
2023-04-19 13:51:35 +0100

    modified keyboard avoiding view

--- src/components/KeyboardAvoidingWrapper.js
+++ src/components/KeyboardAvoidingWrapper.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 //Keyboard Avoiding View
-import { KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback, Keyboard, Dimensions } from 'react-native';
+
+const { width, height } = Dimensions.get('window');
 
 const KeyboardAvoidingWrapper = ({ children }) => {
     return (
-        <KeyboardAvoidingView style={{ flex: 1 }}>
+        <KeyboardAvoidingView keyboardVerticalOffset={height * 0.1} style={{ flex: 1 }}>
             <ScrollView>
                 <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                     {children}
